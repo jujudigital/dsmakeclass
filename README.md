242840da7fa35e98522eb1582d557f1cacaa13a5
Alex Adams <alex@jujudigital.com>
2017-02-04 14:02:03 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# DS Make Class
+
+A simple Node.js based command-line timesaving utility for scaffolding a DS class file.
+
+## Installation
+
+`npm install -g dsmakeclass`
+
+## Usage
+
+In the source folder that you want a new class simply call: `dsmakeclass clasName` and it will add a new class based on a structured template.
+
+---
+
+# Release notes
+## Current version
+0.1.0
+
+### Fixed/Added in this version
+* Initial release
+
+## Change log
+-
+
+---
+# Todo list
+Things that could be implemented in the future:
+
+* Add different templates to support different class structures.
---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+function createTemplate(className){
+  className = className || "";
+  console.log('ClassName:  %s', className);
+
+  var outFile = "./" + className + ".js";
+
+  var fs = require('fs');
+
+  fs.readFile(__dirname+'/templates/classtemplate.js', 'utf8', function(err, data){
+    if ( err ) {
+      return console.log(err);
+    }
+    var result = data.replace(/{ClassName}/g, className);
+
+    fs.writeFile(outFile, result, 'utf8', function(err){
+      if ( err ) return console.log(err);
+    });
+
+  });
+}
+
+
+// Execute script
+program
+  .arguments('<className>')
+  .action(function(className){
+    createTemplate(className);
+  })
+  .parse(process.argv);
